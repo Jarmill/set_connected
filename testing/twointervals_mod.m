33ae03b5d48b99bf6aa61e71d292ce2fec420fc4
Jared Miller <jared.fr.miller@gmail.com>
2022-05-13 13:22:20 +0200

    Sometimes the barrier is feasible?
    xbox = [-1, 1]
    
    X1_alpha = 0.9: infeasible
    X1_alpha = 0.99: feasible
    
    what?

--- testing/twointervals_mod.m
+++ testing/twointervals_mod.m
@@ -16,8 +16,8 @@ e = 0.05;
 % e=0.01;
 epsilon = 1;
 
-xbox = [-1;1];      %works, feasible barrier
-% xbox = [0;1];     %doesn't work, infeasible SDP
+% xbox = [-1;1];      %works, feasible barrier
+xbox = [-0.1;1];     %doesn't work, infeasible SDP
 
 
 T = 1;
@@ -31,8 +31,10 @@ low = xbox(2)*low_alpha+ xbox(1)*(1-low_alpha);
 high = xbox(2)*high_alpha+ xbox(1)*(1-high_alpha);
 
 
-X0_alpha = 0.2;
-X1_alpha = 0.9;
+% X0_alpha = 0.2;
+X0_alpha = 0.05;
+% X1_alpha = 0.9;
+X1_alpha = 0.99;
 
 X0 = xbox(2)*X0_alpha + xbox(1)*(1-X0_alpha);
 X1 = xbox(2)*X1_alpha + xbox(1)*(1-X1_alpha);
@@ -43,6 +45,7 @@ X1 = xbox(2)*X1_alpha + xbox(1)*(1-X1_alpha);
 
 % g = -(1+x)*(-e-x)*(-e+x)*(1-x); % X=[-1,-e]U[e,1]
 g = -(x-xbox(1)).*(xbox(2)-x).*(x-low).*(high-x);
+gf = polyval_func(g, x);
 g0 = -(x-X0)^2; % X0={-(1+e)/2}
 g1 = -(x-X1)^2; % X1={(1+e)/2}
 
@@ -76,7 +79,7 @@ vs = vectorize(sdisplay(double(vc)'*vb));
 dvs = vectorize(sdisplay(double(dvc)'*dvb));
 [t,x] = meshgrid([0,T],xbox',1000);
 
-%% plotting code
+%% plotting of barrier function
 % plot (t,x) -> v(t,x)
 % close all
 figure(1)
@@ -98,6 +101,26 @@ patch(xl_pattern, high*ones(5,1), zl_pattern, 'k', 'FaceAlpha', 0.2, 'EdgeColor'
     
 scatter3(0,X0, fv([0; X0]), 400, 'ko', 'DisplayName', 'X0', 'LineWidth', 3)
 scatter3(T,X1,fv([T; X1]), 400, 'k*', 'DisplayName', 'X1', 'LineWidth', 3)
+
+
+%% plot of support function
+figure(3)
+clf
+Nxsample = 300;
+xrange = linspace(xbox(1), xbox(2), Nxsample);
+
+gx = gf(xrange);
+g0 = gf(X0);
+g1 = gf(X1);
+
+hold on
+plot(xrange, gx)
+scatter(X0, g0, 200, 'ko')
+scatter(X1, g1, 200, 'k*')
+scatter(low, 0, 200, 'ks')
+scatter(high, 0, 200, 'ks')
+plot(xbox, [0,0], '--k')
+xlim(xbox)
 % end
 % figure(2)
 % clf
