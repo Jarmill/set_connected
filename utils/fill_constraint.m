58538c96f264104111a6cc48759ecad8622c7a58
Jared Miller <jared.fr.miller@gmail.com>
2022-09-01 17:11:45 -0400

    allow for a list of inequality constraints to be passed in, without necessitating a struct-type input

--- utils/fill_constraint.m
+++ utils/fill_constraint.m
@@ -3,15 +3,17 @@ function [con_out] = fill_constraint(con_in)
 %con_in should be a struct with fields 'ineq' and 'eq'
 %add them if they are missing
     con_out = con_in;
-    if isempty(con_in) || ~isa(con_in, 'struct')
-        con_out = struct('ineq', [], 'eq', []);
-    end
-    
-    if ~isfield(con_in, 'ineq')
-        con_out.ineq = [];
-    end
-    
-    if ~isfield(con_in, 'eq')
-        con_out.eq = [];
+    if isempty(con_in) 
+        con_out = struct('ineq', [], 'eq', []);        
+    elseif ~isa(con_in, 'struct')
+        con_out = struct('ineq', con_in, 'eq', []);
+    else    
+        if ~isfield(con_in, 'ineq')
+            con_out.ineq = [];
+        end
+
+        if ~isfield(con_in, 'eq')
+            con_out.eq = [];
+        end
     end
 end
